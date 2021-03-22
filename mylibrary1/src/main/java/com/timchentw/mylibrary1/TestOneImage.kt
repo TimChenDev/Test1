72b94871730f61f1f854318b49a1ee34fedc24c3
TimChen <tim@riversense.com>
2021-03-22 11:08:46 +0800

    feat: add TestOneImage

---
+++ mylibrary1/src/main/java/com/timchentw/mylibrary1/TestOneImage.kt
@@ -0,0 +1,12 @@
+package com.timchentw.mylibrary1
+
+import android.content.Context
+import android.support.v7.widget.AppCompatImageView
+
+/**
+ *  @author Tim Chen
+ *  @time   2021/3/22
+ *  @desc
+ */
+class TestOneImage(context: Context?) : AppCompatImageView(context) {
+}
